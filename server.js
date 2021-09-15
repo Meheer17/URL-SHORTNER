31528af3825c65f5eeae5c973818012169fc0e44
Meheer007 <>
2021-09-15 14:42:15 +0000

    whoho

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const urlparser = require('url')
 
-// Basic Configuration
+mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+// Basic Configuratin
 const port = process.env.PORT || 3000;
-
+const schema = mongoose.Schema({url : 'string'});
+const Url = mongoose.model('url', schema);
+console.log(mongoose.connection.readyState)
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -15,8 +25,19 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async function(req, res) {
+        console.log(req.body);
+        const burl = req.body.url;
+
+
+        dns.lookup(burl, callback);
+        const url = new Url({url: burl});
+ 
+  //12.56
+  
+  await url.save((err, data) => {
+          res.json({created: true})
+  })
 });
 
 app.listen(port, function() {
